abe6f31b58a95e640ef035a948c8e4b04b5af3d3
Kirill Kornakov <mtw.mindfactory@mail.ru>
2023-04-20 01:14:15 +0300

    Проставлены модификаторы доступа, удален неиспользуемый код и комментарии, организован порядок свойств и методов в Классе. Выставлены констрейнты для UI-элементов, организован StackView.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,15 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
-   var valueCounterLabel: Int = 0
-   
-    @IBOutlet weak var historyTextView: UITextView!
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var plusButton: UIButton!
-    @IBOutlet weak var minusButton: UIButton!
-    @IBOutlet weak var resetButton: UIButton!
+    // MARK: - IBOutlets
+    
+    @IBOutlet private weak var historyTextView: UITextView!
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var plusButton: UIButton!
+    @IBOutlet private weak var minusButton: UIButton!
+    @IBOutlet private weak var resetButton: UIButton!
 
-    @IBAction func plusChangeLabelButton(_ sender: Any) {
+    // MARK: - Properties
+    
+    private var valueCounterLabel: Int = 0
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        historyTextView.text = "История изменений:"
+        historyTextView.isEditable = false
+        counterLabel.text = "0"
+        plusButton.tintColor = .red
+        minusButton.tintColor = .blue
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction private func plusChangeLabelButton(_ sender: Any) {
         valueCounterLabel += 1
         counterLabel.text = "Значение счётчика: \(valueCounterLabel)"
         historyTextView.text.append(contentsOf: "\n\(getCurrentDate()): значение изменено на +1")
@@ -25,7 +42,7 @@ class ViewController: UIViewController {
         scrollHistoryTextView()
     }
     
-    @IBAction func minusChangeLabelButton(_ sender: Any) {
+    @IBAction private func minusChangeLabelButton(_ sender: Any) {
         guard valueCounterLabel > 0 else {
             valueCounterLabel = 0
             historyTextView.text.append(contentsOf: "\n\(getCurrentDate()): попытка уменьшить значение счётчика ниже 0")
@@ -41,7 +58,7 @@ class ViewController: UIViewController {
         scrollHistoryTextView()
     }
     
-    @IBAction func resetChangeLabelButton(_ sender: Any) {
+    @IBAction private func resetChangeLabelButton(_ sender: Any) {
         guard valueCounterLabel > 0 else { return }
         
         valueCounterLabel = 0
@@ -51,17 +68,9 @@ class ViewController: UIViewController {
         scrollHistoryTextView()
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        historyTextView.text = "История изменений:"
-        historyTextView.isEditable = false
-        counterLabel.text = "0"
-        plusButton.tintColor = .red
-        minusButton.tintColor = .blue
-    }
+    // MARK: - Methods
     
-    func getCurrentDate() -> String {
+    private func getCurrentDate() -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
         
@@ -71,7 +80,7 @@ class ViewController: UIViewController {
         return dateCurrent
     }
     
-    func scrollHistoryTextView() {
+    private func scrollHistoryTextView() {
         let diapason = NSMakeRange(historyTextView.text.count, 0)
         historyTextView.scrollRangeToVisible(diapason)
     }
